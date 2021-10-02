c19a5f7a607d9fb4e28f905fa9064656c3a5ef49
Josh H <josh.terance@gmail.com>
2021-10-02 15:42:23 -0400

    heroku commit

---
+++ commands/utility/github.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'github',
+	description: 'Link to the github to view the bot code.',
+	usage: "",
+	helptext: "",
+	execute(message, args, client) {
+		message.reply('https://github.com/joshtkh/scy-bot');
+	},
+};
